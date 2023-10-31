63d1ab7d342c91c7e3e0e757a1f1e2a04e594ce7
teoseptoskratia <148036798+teoseptoskratia@users.noreply.github.com>
2023-10-31 21:57:12 +0200

    Delete src directory

--- src/tek/client/core.cljs
+++
@@ -1,9 +0,0 @@
-(ns tek.client.core
-  (:require [clojure.browser.repl :as repl]
-            [domina :as dom]
-            [domina.events :as ev]))
-
-(enable-console-print!)
-
-(defn sayHi [] (dom/set-html! (dom/by-id "title") "Hi!"))
-(ev/listen! (dom/by-id "btn") "click" sayHi)
